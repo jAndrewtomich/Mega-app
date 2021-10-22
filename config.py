71f0c22af95adda3b608278e990995e49e525090
andrew <johnatomich@gmail.com>
2021-10-22 05:33:08 -0500

    ADMINS env var

--- config.py
+++ config.py
@@ -12,6 +12,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['johnatomich@gmail.com']
-
+    ADMINS = os.environ.get('ADMINS')
 
