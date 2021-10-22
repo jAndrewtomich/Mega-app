5081731b66be6b6043ca7eebcefe0d602bec3762
andrew <johnatomich@gmail.com>
2021-10-22 10:36:22 -0500

    Add pagination

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get('ADMINS')
+    POSTS_PER_PAGE = 3
 
