ac789fac99c68faba881aa55bc28fd9537b8ae0e
andrew <johnatomich@gmail.com>
2021-11-09 22:11:47 -0600

    configs

--- config.py
+++ config.py
@@ -15,5 +15,6 @@ class Config:
     ADMINS = os.environ.get('ADMINS')
     POSTS_PER_PAGE = 25
     LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 
-
+       
